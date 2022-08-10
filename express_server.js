5dae5fbef54548c7e16e68e50998a0e0f98a4ee2
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-10 22:30:59 +0000

    Add login and logout using cookies

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const cookieParser = require("cookie-parser");
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,7 +11,15 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+// const templateVars = {
+//   username: req.cookies["username"],
+//   // ... any other vars
+// };
+// res.render("urls_index", templateVars);
+
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
@@ -26,8 +36,24 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.post("/urls/:id", (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   urlDatabase[req.params.id] = req.body.longURL; 
+  res.redirect("/urls");
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  // console.log(username);
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  const templateVars = { 
+    username: req.cookies['username']
+  }
+  console.log(templateVars);
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
@@ -44,17 +70,23 @@ app.get("/urls.json", (req, res) => {
 // });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase,
+  username: req.cookies['username'] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies['username']
+  }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id]
-  const templateVars = { id: req.params.id, longURL: longURL };
+  const templateVars = { id: req.params.id, longURL: longURL,
+  username: req.cookies['username'] };
+  console.log("username = ", req.cookies['username']);
   res.render("urls_show", templateVars);
 });
 
