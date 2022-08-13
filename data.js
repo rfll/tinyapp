8730fab42e7c0bdab5e9b3d6a05f84ff010f8f36
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-13 20:46:12 +0000

    Setup data objects and exports to express_server.js

---
+++ data.js
@@ -0,0 +1,20 @@
+const urlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: '1Gdj72'
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: '1Gdj72'
+  }
+};
+const users = {
+  '1Gdj72': {
+    id: '1Gdj72',
+    email: 'email@email.com',
+    password: '$2a$10$.bRtwIYdHqAbd4ZLJNJpHuObUJCI4I/mI/fkrtXIyGY6yEm8oVt/O'
+  }
+};
+
+
+module.exports = { urlDatabase, users };
